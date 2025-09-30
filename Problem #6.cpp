4ce8714b6038fce723f566274e03bf3925adb201
Ahmad-Hiary <233337377+Ahmad-Hiary@users.noreply.github.com>
2025-09-30 18:37:38 +0300

    Transpose ordered matrix .

--- Problem #6.cpp
+++ Problem #6.cpp
@@ -3,11 +3,6 @@
 #include<iomanip>
 
 using namespace std;
-{
-    // Function to generate a random number between from and to
-    int randNum = rand() % (to - from + 1) + from;
-    return randNum;
-}
 
 void Fill3x3MatrixInOrderdNumbers(short x[3][3])
 {
@@ -34,16 +29,40 @@ void PrintMatrix(short x[3][3])
     }
 }
 
+void TransporeMatrix(short x[3][3],short Transposed[3][3])
+{
+    short Count = 1;
+    for (short i = 0; i < 3; i++)
+    {
+        for (short j = 0; j < 3; j++)
+        {
+            Transposed[i][j] = x[j][i];
+            Count++;
+        }
+    }
+}
+
 int main()
 {
     srand((unsigned)time(NULL));
 
     short x[3][3];
+    short Transposed[3][3];
 
     Fill3x3MatrixInOrderdNumbers(x);
 
-    cout << "This is a 3x3 matrix whith random numbers : \n\n";
+    cout << "This is a 3x3 matrix Before transpore : \n\n";
 
     PrintMatrix(x);
 
+
+
+    cout << "This is a 3x3 matrix After transpore : \n\n";
+
+    TransporeMatrix(x, Transposed);
+
+    PrintMatrix(Transposed);
+
 }
+
+void TransporeMatrix(short x[3][3]);
