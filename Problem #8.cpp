876595529e98e98b2e93a8e44031fd40bd744545
Ahmad-Hiary <233337377+Ahmad-Hiary@users.noreply.github.com>
2025-09-30 19:25:10 +0300

    Multiply two matrix at third .

---
+++ Problem #8.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<cstdlib> 
+#include<ctime>   
+#include<cstdio>
+
+using namespace std;
+
+int GetRandomNumber(int from, int to)
+{
+    
+    int randNum = rand() % (to - from + 1) + from;
+    return randNum;
+}
+
+void Fill3x3MatrixWithRandomNumbers(short x[3][3])
+{
+    for (short i = 0; i < 3; i++)
+    {
+        for (short j = 0; j < 3; j++)
+        {
+            x[i][j] = GetRandomNumber(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(short arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf("%0*d   ", 2, arr[i][j]);
+        }
+        cout << "\n";
+    }
+}
+
+void Multiply2MatrixInThird(short Matrix1[3][3], short Matrix2[3][3], short Matrix3[3][3], short rows, short cols)
+{
+    for (short i = 0; i < rows; i++)
+    {
+        for (short j = 0; j < cols; j++)
+        {
+            Matrix3[i][j] = Matrix1[i][j]* Matrix2[i][j];
+           
+        }
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    short Matrix1[3][3];
+    short Matrix2[3][3];
+    short Matrix3[3][3];
+
+    Fill3x3MatrixWithRandomNumbers(Matrix1);
+    Fill3x3MatrixWithRandomNumbers(Matrix2);
+
+    cout << "This is the first matrix : \n\n";
+
+    PrintMatrix(Matrix1,3,3);
+
+    cout << "This is the Second matrix : \n\n";
+
+    PrintMatrix(Matrix2,3,3);
+
+
+
+
+    cout << "This is the Multiplication of matrix 1 and 2 : \n\n";
+
+    Multiply2MatrixInThird(Matrix1,Matrix2,Matrix3,3,3);
+
+    PrintMatrix(Matrix3, 3, 3);
+
+}
+
